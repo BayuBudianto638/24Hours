13df1777e05c2c6a619e5afea55c157dafa4d739
Bayu <Bayu.Budianto@nttindonesia.com>
2022-07-30 23:35:28 +0700

    Add project files.

---
+++ 24Hours/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string str = "12:45:54PM";
+
+int h1 = (int)str[1] - '0';
+int h2 = (int)str[0] - '0';
+int hh = (h2 * 10 + h1 % 10);
+string result = "";
+
+if (str[8] == 'A')
+{
+    if (hh == 12)
+    {
+        result = hh.ToString();
+        for (int i = 2; i <= 7; i++)
+            result = result + str[i];
+    }
+    else
+    {
+        for (int i = 0; i <= 7; i++)
+            result = result + str[i];
+    }
+}
+else
+{
+    if (hh == 12)
+    {
+        result = hh.ToString();
+        for (int i = 2; i <= 7; i++)
+            result = result + str[i];
+    }
+    else
+    {
+        hh = hh + 12;
+        result = hh.ToString();
+        for (int i = 2; i <= 7; i++)
+            result = result + str[i];
+    }
+}
+
+Console.WriteLine($"{result}");
+Console.ReadKey();
